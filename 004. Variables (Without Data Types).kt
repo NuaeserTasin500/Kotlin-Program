8d1c3650ca422fa76ea9c77472481a111e109497
NuaeserTasin500 <osman1711362@gmail.com>
2025-11-21 23:22:14 +0600

    Create 004. Variables (Without Data Types).kt

---
+++ 004. Variables (Without Data Types).kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    // var allows to change value. val doesn't allow allow to change value 
+    var nameVar= "Tasin" 
+    var age = 22
+    println("Hello! My name is $nameVar. My age is $age")
+
+    nameVar = "Rahim"
+    println("Hello! My name is $nameVar. My age is $age")
+    
+    val piName = "Pi Value"
+    val pi = 3.1416 
+    println("Hello! I am $piName. My value is $pi")
+}
